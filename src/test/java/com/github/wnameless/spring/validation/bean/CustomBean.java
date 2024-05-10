d0f5743cf7ae9ea8e42146fa596c09de7e92a791
wmw <wnameless@gmail.com>
2024-05-10 14:07:57 +0800

    Fix message template name

--- src/test/java/com/github/wnameless/spring/validation/bean/CustomBean.java
+++ src/test/java/com/github/wnameless/spring/validation/bean/CustomBean.java
@@ -3,7 +3,7 @@ package com.github.wnameless.spring.validation.bean;
 import com.github.wnameless.spring.validation.SpELScriptAssert;
 
 @SpELScriptAssert(script = "test",
-    message = "{com.github.wnameless.spring.validation.SpELAssert.custom}", reportOn = "test")
+    message = "{com.github.wnameless.spring.validation.SpELScriptAssert.custom}", reportOn = "test")
 public class CustomBean {
 
   public String test = "";
