1d9c17afe3ebc63aaabc360b6ac07ce73377efaa
wmw <wnameless@gmail.com>
2024-05-11 09:16:32 +0800

    Add module-info.java and fix tests

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.github.wnameless.spring.validation.spelscriptassert {
+  requires transitive spring.core;
+  requires transitive spring.context;
+  requires transitive spring.expression;
+  requires transitive spring.beans;
+  requires transitive jakarta.validation;
+
+  exports com.github.wnameless.spring.validation.spelscriptassert;
+}
